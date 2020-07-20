215b04c049f505d226f68ed99eb5c508c2477f07
Jerry Hoogenboom <jerryhoogenboom@outlook.com>
2020-07-20 14:25:23 +0200

    Add Combine Strands option to BGCorrect

--- fdstools/tools/bgcorrect.py
+++ fdstools/tools/bgcorrect.py
@@ -60,7 +60,7 @@ from ..lib.util import nnls
 __version__ = "1.1.0"
 
 
-def get_sample_data(infile, convert_to_raw=False, library=None):
+def get_sample_data(infile, convert_to_raw, library, combine_strands):
     """
     Read data from the given file handle, corresponding to a single
     sample, and fill a dict with all sequences in the sample.
@@ -68,19 +68,22 @@ def get_sample_data(infile, convert_to_raw=False, library=None):
     column_names = infile.readline().rstrip("\r\n").split("\t")
     if column_names == [""]:
         return None, None  # Empty file.
-    column_names.append("forward_noise")
-    column_names.append("reverse_noise")
+    if not combine_strands:
+        column_names.append("forward_noise")
+        column_names.append("reverse_noise")
     column_names.append("total_noise")
-    column_names.append("forward_add")
-    column_names.append("reverse_add")
+    if not combine_strands:
+        column_names.append("forward_add")
+        column_names.append("reverse_add")
     column_names.append("total_add")
-    column_names.append("forward_corrected")
-    column_names.append("reverse_corrected")
+    if not combine_strands:
+        column_names.append("forward_corrected")
+        column_names.append("reverse_corrected")
     column_names.append("total_corrected")
     column_names.append("correction_flags")
     column_names.append("weight")
-    colid_marker, colid_sequence, colid_forward, colid_reverse = get_column_ids(
-        column_names, "marker", "sequence", "forward", "reverse")
+    colid_marker, colid_sequence, colid_forward, colid_reverse, colid_total = get_column_ids(
+        column_names, "marker", "sequence", "forward", "reverse", "total")
     data = {}
     for line in infile:
         cols = line.rstrip("\r\n").split("\t")
@@ -90,17 +93,21 @@ def get_sample_data(infile, convert_to_raw=False, library=None):
                 cols[colid_sequence], "raw", library=library, marker=marker)
         cols[colid_forward] = int(cols[colid_forward])
         cols[colid_reverse] = int(cols[colid_reverse])
+        cols[colid_total] = int(cols[colid_total])
+        if not combine_strands:
+            cols.append(0)
+            cols.append(0)
         cols.append(0)
+        if not combine_strands:
+            cols.append(0)
+            cols.append(0)
         cols.append(0)
-        cols.append(0)
-        cols.append(0)
-        cols.append(0)
-        cols.append(0)
-        cols.append(cols[colid_forward])
-        cols.append(cols[colid_reverse])
-        cols.append(cols[colid_forward] + cols[colid_reverse])
+        if not combine_strands:
+            cols.append(cols[colid_forward])
+            cols.append(cols[colid_reverse])
+        cols.append(cols[colid_total])
         cols.append("not_corrected")
-        cols.append((cols[colid_forward] + cols[colid_reverse]) / 100)
+        cols.append(cols[colid_total] / 100)
         if marker not in data:
             data[marker] = []
         data[marker].append(cols)
@@ -114,14 +121,28 @@ def get_correction_tool_flags(profile_of_allele):
 #get_correction_tool_flags
 
 
-def match_profile(column_names, data, profile, convert_to_raw, library, marker):
-    (colid_marker, colid_sequence, colid_forward, colid_reverse, colid_total, colid_forward_noise,
-     colid_reverse_noise, colid_total_noise, colid_forward_add, colid_reverse_add, colid_total_add,
-     colid_forward_corrected, colid_reverse_corrected, colid_total_corrected,
-     colid_correction_flags, colid_weight) =  get_column_ids(column_names,
-        "marker", "sequence", "forward", "reverse", "total", "forward_noise", "reverse_noise",
-        "total_noise", "forward_add", "reverse_add", "total_add", "forward_corrected",
-        "reverse_corrected", "total_corrected", "correction_flags", "weight")
+def get_noise_matrix(profile, seq_index, strand):
+    Px = np.zeros((len(profile), len(seq_index)))
+    np.fill_diagonal(Px, 100.)
+    for allele in profile:
+        allele_index = seq_index[allele]
+        for sequence in profile[allele]:
+            Px[allele_index, seq_index[sequence]] = profile[allele][sequence][strand]
+    return Px
+#get_noise_matrix
+
+
+def match_profile(column_names, data, profile, convert_to_raw, library, marker, combine_strands):
+    (colid_marker, colid_sequence, colid_forward, colid_reverse, colid_total, colid_total_noise,
+     colid_total_add, colid_total_corrected, colid_correction_flags,
+     colid_weight) = get_column_ids(column_names,
+        "marker", "sequence", "forward", "reverse", "total", "total_noise", "total_add",
+        "total_corrected", "correction_flags", "weight")
+    if not combine_strands:
+        (colid_forward_noise, colid_reverse_noise, colid_forward_add, colid_reverse_add,
+         colid_forward_corrected, colid_reverse_corrected) =  get_column_ids(column_names,
+            "forward_noise", "reverse_noise", "forward_add", "reverse_add", "forward_corrected",
+            "reverse_corrected")
 
     alleles = set(profile)
     other_seqs = set(seq for allele in profile for seq in profile[allele] if seq not in alleles)
@@ -131,21 +152,16 @@ def match_profile(column_names, data, profile, convert_to_raw, library, marker):
     num_seqs = len(seq_index)
 
     # Enter profiles into P.
-    P1 = np.zeros((num_alleles, num_seqs))
-    P2 = np.zeros((num_alleles, num_seqs))
-    np.fill_diagonal(P1, 100.)
-    np.fill_diagonal(P2, 100.)
-    for allele in profile:
-        allele_index = seq_index[allele]
-        for sequence in profile[allele]:
-            sequence_index = seq_index[sequence]
-            P1[allele_index, sequence_index] = profile[allele][sequence]["forward"]
-            P2[allele_index, sequence_index] = profile[allele][sequence]["reverse"]
+    if combine_strands:
+        P = (get_noise_matrix(profile, seq_index, "total"),)
+        C = (np.zeros((1, num_seqs)),)
+    else:
+        P = (get_noise_matrix(profile, seq_index, "forward"),
+            get_noise_matrix(profile, seq_index, "reverse"))
+        C = (np.zeros((1, num_seqs)), np.zeros((1, num_seqs)))
 
     # Enter sample into C.
     used_sequences = set()  # Sequences found in the sample.
-    C1 = np.zeros((1, num_seqs))
-    C2 = np.zeros((1, num_seqs))
     for line in data:
         if line[colid_sequence] in SEQ_SPECIAL_VALUES:
             continue
@@ -161,28 +177,48 @@ def match_profile(column_names, data, profile, convert_to_raw, library, marker):
             # Note: Not adding any new sequences to the profile, since
             # they will just be zeroes and have no effect on the result.
             continue
-        C1[0, sequence_index] = line[colid_forward]
-        C2[0, sequence_index] = line[colid_reverse]
+        if combine_strands:
+            C[0][0, sequence_index] = line[colid_total]
+        else:
+            C[0][0, sequence_index] = line[colid_forward]
+            C[1][0, sequence_index] = line[colid_reverse]
 
     # Stop if this sample has no explicit data for this marker.
     if not used_sequences:
         return
 
     # Compute corrected read counts.
-    A = nnls(np.hstack((P1, P2)).T, np.hstack((C1, C2)).T).T
-    np.fill_diagonal(P1, 0)
-    np.fill_diagonal(P2, 0)
-    forward_noise = A @ P1
-    reverse_noise = A @ P2
-    forward_add = A * P1.sum(1)
-    reverse_add = A * P2.sum(1)
+    A = nnls(np.hstack(P).T, np.hstack(C).T).T
+    noises = []
+    adds = []
+    correcteds = []
+    for Px, Cx in zip(P, C):
+        np.fill_diagonal(Px, 0)
+        noise = A @ Px
+        add = A * Px.sum(1)
+        corrected = Cx - noise
+        corrected[:, :num_alleles] += add
+        noises.append(noise)
+        adds.append(add)
+        correcteds.append(corrected)
+    if combine_strands:
+        colids_noise = (colid_total_noise,)
+        colids_add = (colid_total_add,)
+        colids_corr = (colid_total_corrected,)
+    else:
+        colids_noise = (colid_forward_noise, colid_reverse_noise, colid_total_noise)
+        colids_add = (colid_forward_add, colid_reverse_add, colid_total_add)
+        colids_corr = (colid_forward_corrected, colid_reverse_corrected, colid_total_corrected)
+        noises.append(sum(noises))
+        adds.append(sum(adds))
+        correcteds.append(sum(correcteds))
 
     # Round values to 3 decimal positions.
     A.round(3, A)
-    forward_noise.round(3, forward_noise)
-    reverse_noise.round(3, reverse_noise)
-    forward_add.round(3, forward_add)
-    reverse_add.round(3, reverse_add)
+    for noise, add, corrected in zip(noises, adds, correcteds):
+        noise.round(3, noise)
+        add.round(3, add)
+        corrected.round(3, corrected)
 
     for line in data:
         if line[colid_sequence] in SEQ_SPECIAL_VALUES:
@@ -192,20 +228,13 @@ def match_profile(column_names, data, profile, convert_to_raw, library, marker):
         except KeyError:
             line[colid_correction_flags] = "not_in_ref_db"
             continue
-        line[colid_forward_noise] = forward_noise[0, i]
-        line[colid_reverse_noise] = reverse_noise[0, i]
-        line[colid_total_noise] = forward_noise[0, i] + reverse_noise[0, i]
-        line[colid_forward_corrected] -= line[colid_forward_noise]
-        line[colid_reverse_corrected] -= line[colid_reverse_noise]
-        line[colid_total_corrected] -= line[colid_total_noise]
+        for colid_noise, colid_corrected, noise, corrected in zip(colids_noise, colids_corr, noises, correcteds):
+            line[colid_noise] = noise[0, i]
+            line[colid_corrected] = corrected[0, i]
         if i < num_alleles:
-            line[colid_forward_add] = forward_add[0, i]
-            line[colid_reverse_add] = reverse_add[0, i]
-            line[colid_total_add] = forward_add[0, i] + reverse_add[0, i]
-            line[colid_forward_corrected] += line[colid_forward_add]
-            line[colid_reverse_corrected] += line[colid_reverse_add]
-            line[colid_total_corrected] += line[colid_total_add]
-            line[colid_correction_flags] = get_correction_tool_flags(profile[sequence])
+            for colid_add, colid_corrected, add in zip(colids_add, colids_corr, adds):
+                line[colid_add] = add[0, i]
+            line[colid_correction_flags] = get_correction_tool_flags(profile[line[colid_sequence]])
             line[colid_weight] = A[0, i]
         else:
             line[colid_correction_flags] = "corrected_as_background_only"
@@ -215,29 +244,19 @@ def match_profile(column_names, data, profile, convert_to_raw, library, marker):
     for sequence, i in seq_index.items():
         if sequence in used_sequences:
             continue
-        amount = forward_noise[0, i] + reverse_noise[0, i]
-        if i < num_alleles:
-            amount += forward_add[0, i] + reverse_add[0, i]
-        if amount > 0:
+        if correcteds[-1][0, i] > 0:
             line = [""] * len(column_names)
             line[colid_marker] = marker
             line[colid_sequence] = sequence
             line[colid_forward] = 0
             line[colid_reverse] = 0
             line[colid_total] = 0
-            line[colid_forward_noise] = forward_noise[0, i]
-            line[colid_reverse_noise] = reverse_noise[0, i]
-            line[colid_total_noise] = forward_noise[0, i] + reverse_noise[0, i]
-            line[colid_forward_corrected] = -line[colid_forward_noise]
-            line[colid_reverse_corrected] = -line[colid_reverse_noise]
-            line[colid_total_corrected] = -line[colid_total_noise]
+            for colid_noise, colid_corrected, noise, corrected in zip(colids_noise, colids_corr, noises, correcteds):
+                line[colid_noise] = noise[0, i]
+                line[colid_corrected] = corrected[0, i]
             if i < num_alleles:
-                line[colid_forward_add] = forward_add[0, i]
-                line[colid_reverse_add] = reverse_add[0, i]
-                line[colid_total_add] = forward_add[0, i] + reverse_add[0, i]
-                line[colid_forward_corrected] += line[colid_forward_add]
-                line[colid_reverse_corrected] += line[colid_reverse_add]
-                line[colid_total_corrected] += line[colid_total_add]
+                for colid_add, colid_corrected, add in zip(colids_add, colids_corr, adds):
+                    line[colid_add] = add[0, i]
                 line[colid_correction_flags] = get_correction_tool_flags(profile[sequence])
                 line[colid_weight] = A[0, i]
             else:
@@ -250,8 +269,8 @@ def match_profile(column_names, data, profile, convert_to_raw, library, marker):
 #match_profile
 
 
-def match_profiles(infile, outfile, profiles, library, seqformat):
-    column_names, data = get_sample_data(infile, convert_to_raw=seqformat=="raw", library=library)
+def match_profiles(infile, outfile, profiles, library, seqformat, combine_strands):
+    column_names, data = get_sample_data(infile, seqformat=="raw", library, combine_strands)
     if column_names is None:
         return  # Empty file.
     colid_sequence = get_column_ids(column_names, "sequence")
@@ -260,7 +279,7 @@ def match_profiles(infile, outfile, profiles, library, seqformat):
     for marker in data:
         if marker in profiles:
             match_profile(column_names, data[marker], profiles[marker],
-                          seqformat != "raw", library, marker)
+                          seqformat != "raw", library, marker, combine_strands)
         for line in data[marker]:
             if seqformat is not None and seqformat != "raw":
                 line[colid_sequence] = ensure_sequence_format(
@@ -273,6 +292,9 @@ def add_arguments(parser):
     parser.add_argument("profiles", metavar="PROFILES", type=argparse.FileType("tr"),
         help="file containing background noise profiles to match")
     add_input_output_args(parser, single_in=True, batch_support=True, report_out=False)
+    parser.add_argument("-C", "--combine-strands", action="store_true",
+        help="if specified, stutter noise correction will be done on the total number of reads, "
+             "instead of separately for either strand")
     filtergroup = parser.add_argument_group("filtering options")
     filtergroup.add_argument("-M", "--marker", metavar="MARKER", help="work only on MARKER")
     add_sequence_format_args(parser)
@@ -299,7 +321,8 @@ def run(args):
             raise ValueError("multiple input files for sample '%s' specified " % tag)
         try:
             infile = sys.stdin if infiles[0] == "-" else open(infiles[0], "tr")
-            match_profiles(infile, outfile, profiles, args.library, args.sequence_format)
+            match_profiles(infile, outfile, profiles, args.library, args.sequence_format,
+                args.combine_strands)
             if infile != sys.stdin:
                 infile.close()
         except IOError as e:
