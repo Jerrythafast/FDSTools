2eae74465addbdd28588631f4a7cb1c86fc483a1
Jerry Hoogenboom <jerryhoogenboom@outlook.com>
2021-05-12 13:10:54 +0200

    Samplestats: fix missing import

--- fdstools/tools/samplestats.py
+++ fdstools/tools/samplestats.py
@@ -59,7 +59,7 @@ import sys
 
 from errno import EPIPE
 
-from ..lib.cli import add_input_output_args, get_input_output_files
+from ..lib.cli import add_input_output_args, get_input_output_files, pos_int_arg
 from ..lib.io import get_column_ids
 from ..lib.library import get_max_expected_alleles
 from ..lib.seq import SEQ_SPECIAL_VALUES, ensure_sequence_format
