c9f54eb7e798644cb7c58159b240afb76955a5b6
Jerry Hoogenboom <jerryhoogenboom@outlook.com>
2021-11-02 18:22:38 +0100

    Add microhaplotype positions to STRNaming options

--- fdstools/lib/library.py
+++ fdstools/lib/library.py
@@ -294,7 +294,7 @@ def parse_library(handle):
                         section for section in MUTEX_GROUPS[group] if section in settings))
                     for group in groups)))
         options = {option: settings[option] for option in {"flanks", "max_expected_copies",
-                "expected_allele_length"} & settings.keys()}
+                "expected_allele_length", "microhaplotype_positions"} & settings.keys()}
         if "explicit STR" in groups:
             # Legacy FDSTools-style definition of an STR marker.
             # TODO: Alias of STR markers was defined as excluding the prefix/suffix!
